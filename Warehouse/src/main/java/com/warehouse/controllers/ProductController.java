346faf81c830505ea865190a5d3126984196e7a6
Dakubik <kubikdavid0@gmail.com>
2023-09-28 14:42:22 +0200

    created new endpoints and template for editing the product fields

--- Warehouse/src/main/java/com/warehouse/controllers/ProductController.java
+++ Warehouse/src/main/java/com/warehouse/controllers/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -28,7 +29,6 @@ public class ProductController {
 
     @GetMapping("/add")
     public String addProduct(Model model) {
-
         model.addAttribute("products", new Product());
 
         return "addProduct";
@@ -36,9 +36,43 @@ public class ProductController {
 
     @PostMapping("/add")
     public String submitProduct(@ModelAttribute Product product) {
-
         productRepository.save(product);
 
         return "redirect:/warehouse";
     }
+
+    @GetMapping("/edit/{productId}")
+    public String editProduct(@PathVariable Long productId, Model model) {
+        Product product = productRepository.findById(productId).orElse(null);
+
+        if (product != null) {
+            model.addAttribute("product", product);
+            return "editProduct";
+        } else {
+            return "redirect:/warehouse";
+        }
+    }
+
+    @PostMapping("/edit/{productId}")
+    public String submitEditedProduct(
+            @PathVariable Long productId,
+            @ModelAttribute Product updatedProduct) {
+
+        Product product = productRepository.findById(productId).orElse(null);
+
+        if (product != null) {
+
+            product.setName(updatedProduct.getName());
+            product.setDescription(updatedProduct.getDescription());
+            product.setPrice(updatedProduct.getPrice());
+            product.setStock(updatedProduct.getStock());
+            product.setEanCode(updatedProduct.getEanCode());
+
+            productRepository.save(product);
+            return "redirect:/warehouse";
+        } else {
+            return "redirect:/warehouse";
+        }
+    }
+
 }
