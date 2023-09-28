f267aa062c7bd7d5cf025a822e596bf8f4b6e760
Dakubik <kubikdavid0@gmail.com>
2023-09-28 13:40:07 +0200

    create template for adding new product and new endpoints for that

--- Warehouse/src/main/java/com/warehouse/controllers/ProductController.java
+++ Warehouse/src/main/java/com/warehouse/controllers/ProductController.java
@@ -5,6 +5,8 @@ import com.warehouse.repositories.ProductRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ProductController {
@@ -18,10 +20,25 @@ public class ProductController {
 
     @GetMapping("/warehouse")
     public String getWarehouse(Model model) {
-
         Iterable<Product> products = productRepository.findAll();
         model.addAttribute("products", products);
 
         return "warehouse";
+    }
+
+    @GetMapping("/add")
+    public String addProduct(Model model) {
+
+        model.addAttribute("products", new Product());
+
+        return "addProduct";
+    }
+
+    @PostMapping("/add")
+    public String submitProduct(@ModelAttribute Product product) {
+
+        productRepository.save(product);
+
+        return "redirect:/warehouse";
     }
 }
--- Warehouse/src/main/java/com/warehouse/models/Product.java
+++ Warehouse/src/main/java/com/warehouse/models/Product.java
@@ -1,8 +1,8 @@
 package com.warehouse.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,16 +10,25 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @Entity
 public class Product {
 
     @Id
+    @GeneratedValue
     private Long id;
     private String name;
     private String description;
-    private Long price;
+    private int price;
     private String currency = "€";
     private Long stock;
     private Long eanCode;
+
+    public Product(String name, String description, int price, String currency, Long stock, Long eanCode) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.currency = currency;
+        this.stock = stock;
+        this.eanCode = eanCode;
+    }
 }
