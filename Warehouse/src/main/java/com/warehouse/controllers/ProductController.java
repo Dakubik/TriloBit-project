2a1dd272fc57f7144ad7ed0af41b808e100a94af
Dakubik <kubikdavid0@gmail.com>
2023-09-28 12:14:56 +0200

    created first endpoint to list all products

--- Warehouse/src/main/java/com/warehouse/controllers/ProductController.java
+++ Warehouse/src/main/java/com/warehouse/controllers/ProductController.java
@@ -1,13 +1,27 @@
 package com.warehouse.controllers;
 
+import com.warehouse.models.Product;
+import com.warehouse.repositories.ProductRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProductController {
 
+    private final ProductRepository productRepository;
+
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+
     @GetMapping("/warehouse")
-    public String getWarehouse() {
-        
+    public String getWarehouse(Model model) {
+
+        Iterable<Product> products = productRepository.findAll();
+        model.addAttribute("product", products);
+
+        return "warehouse";
     }
 }
